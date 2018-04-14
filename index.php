ba88000194137bc01b3e99bbc30cf572b34e6732
STUDENT3-4\Student_3-4 <rtempavljuk19@gmail.com>
2018-04-14 18:58:41 +0300

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-	echo "hello";
+	class MainController {
+		public function index()//
+		{	
+			$model = new MainModel;//создать данные модель
+			$data = $model->getData();//вызов данных. Мы берем данные у модели
+			include 'view.php';//выводим.
+			//получение данных идет в контролере.
+
+		}
+	}
+
+	class MainModel {
+		public function getData()
+		{
+			return $this->getDataFromDatabase();
+		}
+		private function getDataFromDatabase() {
+			return 'Data from MainModel!';
+		}
+	}
+	//router Обращается к котролеру
+	$controller = new MainController;//контролер
+	call_user_func([$controller, 'index']);//
+
 ?>
